529760bdcecee2bde67dd9cdf86ebea51b5b1a34
hiren-dholariya <hirendholariya11@ymail.com>
2026-05-01 20:16:54 +0530

    new page add

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
+/* ---- main menu dropdown --- */
+$(document).ready(function () {
+    $('.nav-item.dropdown').each(function () {
+        let $item = $(this);
+        let $btn = $item.find('button');
+        let $menu = $item.find('.dropdown-menu');
+
+        if ($btn.length && $menu.length) {
+            $btn.on('click', function () {
+                $menu.toggleClass('show');
+            });
+        }
+    });
+});
+
+
 /*===== go to top ===========*/
     $(window).scroll(function () {
         if ($(this).scrollTop() >= 100) {
@@ -8,14 +24,7 @@
     });
     $('#return-to-top').click(function () { $('body,html').animate({ scrollTop: 0 }, 500); });
 
-/* ----- AOS ----- */
-AOS.init({
-    easing: 'ease-out-back',
-    duration: 1000
-});
 
-
-/* ----- # AOS ----- */
 
 /* ==== fully year show */
 var date = new Date().getFullYear();document.getElementById("year").innerHTML = date;
